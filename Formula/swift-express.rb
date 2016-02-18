999f61b4d69c92f28b642d3dd5ebcb05d924a1b9
Yehor Popovych <popovych.yegor@gmail.com>
2016-02-18 18:40:49 +0200

    Moved to my repo

--- Formula/swift-express.rb
+++ Formula/swift-express.rb
@@ -2,7 +2,7 @@ class SwiftExpress < Formula
   desc "Command line project manager for Swift Express"
   homepage "http://swiftexpress.io/"
   version "0.1.4"
-  url "https://github.com/crossroadlabs/ExpressCommandLine.git", :tag => version
+  url "https://github.com/ypopovych/ExpressCommandLine.git", :tag => version
   
   head "https://github.com/crossroadlabs/ExpressCommandLine.git", :branch => "master"
 
