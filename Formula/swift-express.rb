f28d3b5c677228fc5c454e1863273a345bcb8f67
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-04-02 16:01:13 +0300

    New swift-express formula

--- Formula/swift-express.rb
+++ Formula/swift-express.rb
@@ -1,24 +1,18 @@
 class SwiftExpress < Formula
   desc "Command line project manager for Swift Express"
   homepage "http://swiftexpress.io/"
-  version "0.2.2"
+  version "0.3.0"
   url "https://github.com/crossroadlabs/ExpressCommandLine.git", :tag => version
   
   head "https://github.com/crossroadlabs/ExpressCommandLine.git", :branch => "master"
 
-  depends_on :xcode => "7.2"
-  depends_on :macos => :yosemite
+  depends_on :xcode => "8.3"
+  depends_on :macos => :el_capitan
 
-  depends_on 'openssl'
-  depends_on 'libevhtp' => ["without-oniguruma", "with-shared"]
-  depends_on 'carthage' 
+  depends_on 'libevhtp' => ["without-oniguruma", "with-shared", "without-openssl"]
 
   def install
-    system "carthage", "update", "--platform", "Mac", "--no-use-binaries", "--no-build"
-    system "rm", "-rf", "Carthage/Checkouts/Commandant/Carthage/Checkouts/Nimble", "Carthage/Checkouts/Commandant/Carthage/Checkouts/Quick"
-    system "carthage", "build", "--platform", "Mac"
-    xcodebuild "SYMROOT=build"
-    libexec.install "build/Release/swift-express.app"
-    bin.install "swift-express/Scripts/swift-express"
+    system "swift", "build", "-c", "release"
+    bin.install ".build/release/swift-express"
   end
 end
